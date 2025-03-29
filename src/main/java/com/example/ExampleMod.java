0951c3a7e461b61987e8b06386b0542acbe69855
TheFirstWither <74547695+BlackGhostTim@users.noreply.github.com>
2025-03-29 20:00:59 +0300

    1111

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -2,15 +2,15 @@ package com.example;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "ineedurtoken", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
-		// Below is a demonstration of an access-transformed class access.
-        System.out.println("Color State: " + new GlStateManager.Color());
+        ClientCommandHandler.instance.registerCommand(new gmt());
     }
 }
